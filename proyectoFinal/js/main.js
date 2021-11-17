cdff5c1af726fde63bac47d5fc5f2b3c7dd05691
Nancy Martinez <zoe.arg@gmail.com>
2021-11-17 01:37:44 -0300

    Contenido de index, refinamiento estilos, agregado de 1 producto

--- proyectoFinal/js/main.js
+++ proyectoFinal/js/main.js
@@ -1,6 +1,6 @@
 /*------------------------Comienzo MAIN------------------------ */
 //manejo inicial de variables para almacenar los precios
-const servBaseDisponibles=[ new servicioBase(1,"Esculpidas",2000),new servicioBase(2,"Semipermanente",900),];
+const servBaseDisponibles=[ new servicioBase(1,"Esculpidas",2000),new servicioBase(2,"Semipermanente",900),new servicioBase(3,"Postizas",1000)];
 const servAdicionalDisponibles= [new ServicioAdicional(1,"Nailart simple",250,500),
                                         new ServicioAdicional(2,"Ilustración de personaje",1000,5000),
                                         new ServicioAdicional(3,"Full glitter",250,400),
